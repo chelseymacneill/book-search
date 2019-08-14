eccfbc4e7a7428bebe6954f78983109824702a01
cmac <chelsey.macneill@gmail.com>
2019-08-13 18:48:34 -0700

    Connected the express server with the React server

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# book-search
+book-search
 A React app that allows you to search for books and save ones you want to purchase later.
---
+++ client/build/precache-manifest.44cc8dfac54ce2fbc8c48e5135dfe6fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "df4caa1158ffb72ea2f0",
+    "url": "/static/js/main.5c70d4d8.chunk.js"
+  },
+  {
+    "revision": "dc1e423360fd161ec210",
+    "url": "/static/js/2.11d9e56a.chunk.js"
+  },
+  {
+    "revision": "df4caa1158ffb72ea2f0",
+    "url": "/static/css/main.1c02aaa9.chunk.css"
+  },
+  {
+    "revision": "1867bbb00e7d3f764ff8566a4bd12a3c",
+    "url": "/index.html"
+  }
+];
